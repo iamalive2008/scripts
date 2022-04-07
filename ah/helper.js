cb0df5659b5345ea8a6e9f1d800a8ad8937e5242
wangye.dev <wangye.dev@bytedance.com>
2022-04-07 15:29:58 +0800

    WIP

---
+++ ah/helper.js
@@ -0,0 +1,13 @@
+
+$(function () {
+    console.log("AHHelper JS Started!")
+    setInterval(function () {
+        worker()
+    }, 3000);
+
+
+    function worker() {
+        console.log("workder")
+    }
+});
+
